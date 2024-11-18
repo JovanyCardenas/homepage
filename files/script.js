4b8a842bd331ff36f8f5bd8fc3727327459fc860
Jovany Cardenas Vargas <16jovany2004@gmail.com>
2024-11-18 12:40:32 -0800

    update

--- files/script.js
+++ files/script.js
@@ -6,13 +6,18 @@ function dropdownContentButton(name){
         content.style.display = "none";
     }
 }
-function login(username, password){
+function login(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-
-    if (username === ""){
-        if (username === "" && password === ""){
+    if (username === "jovany.cardena"){
+        if (username === "jovany.cardena" && password === "adminUser2014"){
             // Welcome user logged in!
-        }else{}
-        
+            console.log("Correct Credentials! Welcome: " + username);
+            let x = "https://jovanycardenasvargas.org/dashboard/" + username;
+            window.open(x);
+        }else{
+            console.error("Invalid Credentials!!!");
+        }
     }
 }
